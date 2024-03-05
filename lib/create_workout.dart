959f2bc994f440dc5a9f447d5174e747e9454bc7
ljarrett <ljarrett@uoguelph.ca>
2024-03-05 16:19:57 -0500

    fix

--- lib/create_workout.dart
+++ lib/create_workout.dart
@@ -1,22 +1,46 @@
 import 'package:flutter/material.dart';
+import 'main.dart'; // Ensure this import is correct to access BottomNavBar
+import 'navbar.dart';
+class CreateWorkoutPage extends StatefulWidget {
+  @override
+  _CreateWorkoutPageState createState() => _CreateWorkoutPageState();
+}
 
-class CreateWorkout extends StatelessWidget {
+class _CreateWorkoutPageState extends State<CreateWorkoutPage> {
+  int _selectedIndex = 1;
+
+  void _onNavigate(String routeName) {
+    if (ModalRoute.of(context)?.settings.name != routeName) {
+      Navigator.pushNamed(context, routeName);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Create Workout'),
       ),
-      body: ListView(
-        children: <Widget>[
-          ListTile(
-            leading: Icon(Icons.person),
-            title: Text('Bruh'),
-            onTap: () {
-              // Handle the tap, possibly navigate to the Account settings page
-            },
-          ),
-        ],
+      body: Center(
+        child: Column(
+          // Align the column's children to the center of the screen
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Create your workout here'),
+            SizedBox(height: 20), // Provides some spacing between the text and the button
+            ElevatedButton(
+              onPressed: () {
+                // Navigate to the Edit Workout page when the button is pressed
+                Navigator.pushNamed(context, '/editWorkout');
+              },
+              child: Text('Create Workout'), // Set the button text
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavBar(
+        selectedIndex: _selectedIndex,
+        onItemSelected: _onNavigate,
       ),
     );
   }
---
+++ lib/navbar.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class BottomNavBar extends StatelessWidget {
+  final int selectedIndex;
+  final Function(String) onItemSelected;
+
+  BottomNavBar({required this.selectedIndex, required this.onItemSelected});
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.auto_graph),
+          label: 'Stats',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.fitness_center),
+          label: 'Create Workout',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.settings),
+          label: 'Settings',
+        ),
+      ],
+      currentIndex: selectedIndex,
+      onTap: (index) {
+        switch (index) {
+          case 0:
+            onItemSelected('/stats');
+            break;
+          case 1:
+            onItemSelected('/createWorkout');
+            break;
+          case 2:
+            onItemSelected('/settings');
+            break;
+        }
+      },
+      selectedItemColor: Colors.amber[800],
+      unselectedItemColor: Colors.grey,
+      type: BottomNavigationBarType.fixed,
+
+    );
+  }
+}
+
