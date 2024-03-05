6898c91b34a7393b7ece45dfc45358676f102696
Ameer <ameermushani@gmail.com>
2024-03-05 16:27:21 -0500

    Add calendar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 import 'settings.dart';
 import 'create_workout.dart';
 import 'stats.dart';
@@ -30,6 +31,9 @@ class LandingPage extends StatefulWidget {
 
 class _LandingPageState extends State<LandingPage> {
   int _selectedIndex = 1;
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
 
   void _onNavigate(String routeName) {
     if (ModalRoute.of(context)?.settings.name != routeName) {
@@ -44,7 +48,30 @@ class _LandingPageState extends State<LandingPage> {
         title: Text('Workout App'),
       ),
       body: Center(
-        child: Text('LETS MAKE SOME DELTA GAINS!'),
+        child:  TableCalendar(
+
+          firstDay: DateTime.utc(2000, 1, 1),
+          lastDay: DateTime(2100, 12, 31),
+          focusedDay: DateTime.now(),
+          calendarFormat: _calendarFormat,
+          selectedDayPredicate: (day) {
+            return isSameDay(_selectedDay, day);
+          },
+          onDaySelected: (selectedDay, focusedDay) {
+            setState(() {
+              _selectedDay = selectedDay;
+              _focusedDay = focusedDay; // update `_focusedDay` here as well
+            });
+          },
+          onFormatChanged: (format) {
+            setState(() {
+              _calendarFormat = format;
+            });
+          },
+          onPageChanged: (focusedDay) {
+            _focusedDay = focusedDay;
+          },
+        )
       ),
       bottomNavigationBar: BottomNavBar(
         selectedIndex: _selectedIndex,
