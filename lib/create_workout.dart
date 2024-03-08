7882477df777b0a42f6aad320a07f98428874352
Ameer <ameermushani@gmail.com>
2024-03-08 12:33:05 -0500

    Add provider and display of workouts

--- lib/create_workout.dart
+++ lib/create_workout.dart
@@ -1,6 +1,13 @@
+import 'dart:ui';
+
+import 'package:expansion_tile_card/expansion_tile_card.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'main.dart'; // Ensure this import is correct to access BottomNavBar
 import 'navbar.dart';
+import 'workout_provider.dart';
+import 'workout.dart';
+import 'exercise.dart';
 class CreateWorkoutPage extends StatefulWidget {
   @override
   _CreateWorkoutPageState createState() => _CreateWorkoutPageState();
@@ -21,22 +28,53 @@ class _CreateWorkoutPageState extends State<CreateWorkoutPage> {
       appBar: AppBar(
         title: Text('Create Workout'),
       ),
-      body: Center(
-        child: Column(
-          // Align the column's children to the center of the screen
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('Create your workout here'),
-            SizedBox(height: 20), // Provides some spacing between the text and the button
-            ElevatedButton(
-              onPressed: () {
-                // Navigate to the Edit Workout page when the button is pressed
-                Navigator.pushNamed(context, '/editWorkout');
-              },
-              child: Text('Create Workout'), // Set the button text
+      body: Consumer<WorkoutProvider>(
+        builder: (context, workoutProvider, child) {
+          return Center(
+            child: Column(
+              // Align the column's children to the center of the screen
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(height: 20), // Provides some spacing between the text and the button
+                ElevatedButton(
+                  onPressed: () {
+                    // Navigate to the Edit Workout page when the button is pressed
+                    Navigator.pushNamed(context, '/editWorkout');
+                  },
+                  child: Text('Create Workout'), // Set the button text
+                ),
+                Expanded(child: ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: workoutProvider.workouts.length,
+                    itemBuilder: (context, i) {
+                      Workout curWorkout = workoutProvider.workouts[i];
+                      return ExpansionTileCard(
+                        title: Text(curWorkout.name),
+                        children: <Widget>[
+                          ListView.builder(
+                              shrinkWrap: true,
+                              physics: NeverScrollableScrollPhysics(),
+                              scrollDirection: Axis.vertical,
+                              itemCount: curWorkout.exercises.length,
+                              itemBuilder: (context, j) {
+                                Exercise curExer = curWorkout.exercises[j];
+                                return Column(children: [
+                                  Text("Exercise: ${curExer.name}", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),),
+                                  Text("Sets: ${curExer.sets}"),
+                                  Text("Reps: ${curExer.reps}"),
+                                  Text("Weight: ${curExer.weight}"),
+                                  SizedBox(height: 20,)
+                                ],);
+                              }
+                          ),
+                        ],
+                      );
+                    }
+                ) )
+              ],
             ),
-          ],
-        ),
+          );
+        },
       ),
       bottomNavigationBar: BottomNavBar(
         selectedIndex: _selectedIndex,
